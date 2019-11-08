4292da938df648b8a8c687d542430772084e7aa5
vwaurich <volker.waurich@gmx.de>
2019-11-08 13:55:26 +0100

    threaded  main loop

--- Modelica_DeviceDrivers/Blocks/Examples/TestOPC_UA_Server.mo
+++ Modelica_DeviceDrivers/Blocks/Examples/TestOPC_UA_Server.mo
@@ -1,6 +1,9 @@
 within Modelica_DeviceDrivers.Blocks.Examples;
 model TestOPC_UA_Server
   extends Modelica.Icons.Example;
-  Communication.OPC_UA_Server oPC_UA_Server1 annotation(
+  Communication.OPC_UA_Server oPC_UA_Server1 annotation (
     Placement(visible = true, transformation(origin = {-74, -84}, extent = {{-10, -10}, {10, 10}}, rotation = 0)));
+  OperatingSystem.SynchronizeRealtime synchronizeRealtime
+    annotation (Placement(transformation(extent={{-82,-18},{-62,2}})));
+  annotation (experiment(StopTime=10));
 end TestOPC_UA_Server;
--- Modelica_DeviceDrivers/Resources/Include/MDDopcuaServer.h
+++ Modelica_DeviceDrivers/Resources/Include/MDDopcuaServer.h
@@ -13,31 +13,75 @@
 #include "../thirdParty/open62541/open62541.h"
 #include "ModelicaUtilities.h"
 
+#if !defined(WIN32_LEAN_AND_MEAN)
+#define WIN32_LEAN_AND_MEAN
+#endif
+#include <windows.h>
+#include "../src/include/CompatibilityDefs.h"
+
+
+
 static volatile UA_Boolean running = true;
 
 typedef struct{
+  HANDLE hThread;
+  CRITICAL_SECTION receiveLock;
+
   UA_Server* server;
   UA_Boolean running;
 } MDDopcuaServer;
+
+
+DWORD WINAPI MDD_OPCUAServerThread(LPVOID p_opcua) {
+    MDDopcuaServer * opcua = (MDDopcuaServer *) p_opcua;
+	ModelicaFormatMessage("Create server\n");
+	UA_StatusCode retval = UA_Server_run(opcua->server, &opcua->running);
+
+    return 0;
+}
+
+
 
 void* MDD_opcuaServerConstructor()
 {
 	    ModelicaFormatMessage("Create server\n");
 		MDDopcuaServer* opcua = (MDDopcuaServer*) calloc(sizeof(MDDopcuaServer), 1);
 
+		// init open62541 opc ua server
 		opcua->server = UA_Server_new();
 		opcua->running = true;
 		UA_ServerConfig_setDefault(UA_Server_getConfig(opcua->server));	
-
-		UA_StatusCode retval = UA_Server_run(opcua->server, &opcua->running);
-
+        
+		//start main loop in a separate thread
+		DWORD id1;
+		InitializeCriticalSection(&opcua->receiveLock);
+        opcua->hThread = CreateThread(0, 0, MDD_OPCUAServerThread, opcua, 0, &id1);
+		if (!opcua->hThread) {
+                DeleteCriticalSection(&opcua->receiveLock);
+				opcua->running = false;
+				UA_Server_delete(opcua->server);
+                free(opcua);
+                opcua = NULL;
+                ModelicaError("MDDOPCUA_Server.h: Error creating thread for main loop.\n");
+            }
 		ModelicaFormatMessage("Server Created\n");
-	return NULL;
+	return (void*)opcua;
 }
 
-void MDD_opcuaServerDestructor(void* p_opcuaServer)
+void MDD_opcuaServerDestructor(void* p_opcua)
 {
-	//UA_Server_destroy(server);
+	MDDopcuaServer * opcua = (MDDopcuaServer *) p_opcua;
+	opcua->running = false;
+	UA_Server_delete(opcua->server);
+	if (opcua->hThread) {
+		DWORD dwEc = 1;
+		WaitForSingleObject(opcua->hThread, 1000);
+		if (GetExitCodeThread(opcua->hThread, &dwEc) && dwEc == STILL_ACTIVE) {
+			TerminateThread(opcua->hThread, 1);
+		}
+		CloseHandle(opcua->hThread);
+		DeleteCriticalSection(&opcua->receiveLock);
+	}
 	ModelicaFormatMessage("Destroy server\n");
 }
 
